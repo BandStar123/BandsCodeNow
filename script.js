1c519d1dd32cb18951497c29ed2805b22165cb8d
BandStar123 <likeic3@gmail.com>
2025-11-21 12:36:00 -0500

    Added in proper discord flask cors  for the keys

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ function generateKey() {
         
         generatedKeys.add(key);
         // Notify Discord bot (replace URL with your actual webhook endpoint)
-        fetch('https://YOUR_DISCORD_BOT_ENDPOINT_HERE', {
+        fetch('http://localhost:5000/notify_key', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ key })
